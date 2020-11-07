7f13dcc068f164cb958e4db03e8fadc1043fb217
Eli Stone <3li.stone@gmail.com>
2020-11-07 12:22:11 +0000

    Adding pulse for timer when not running.

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <div class="timer-container sticky-top mt-n4 mb-2">
+    <div class="timer-container sticky-top mb-2">
       <div class="container">
         <div class="row">
           <div class="col text-center">
             <div class="timer-spinner-holder">
               <div class="timer-spinner-wrapper">
-                <b-spinner v-if="running"
-                           class="timer-spinner"
+                <b-spinner class="timer-spinner"
                            :label="current + 'seconds'"
-                           :variant="colours">
+                           :variant="colours"
+                           :type="[running ? 'border' : 'grow']">
                 </b-spinner>
-                <span class="timer-numbers text-black">{{current}}</span>
+                <span class="timer-numbers text-black">{{ current }}</span>
               </div>
             </div>
           </div>
@@ -38,7 +38,7 @@ export default {
   props: {
     start: {
       type: Number,
-      default: 15,
+      default: 99,
     },
     current: {
       type: Number,
@@ -54,8 +54,8 @@ export default {
   },
   computed: {
     /**
-       * calculate the percentage handling any errors
-       */
+     * calculate the percentage handling any errors
+     */
     percentage() {
       let { start, current } = this;
 
@@ -75,10 +75,13 @@ export default {
     },
 
     /**
-       * The colours for the timer
-       * @returns {string}
-       */
+     * The colours for the timer
+     * @returns {string}
+     */
     colours() {
+      if (!this.running) {
+        return 'light';
+      }
       if (this.percentage > 66) {
         return 'success';
       }
@@ -94,47 +97,47 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  .timer-container {
-    /*min-height: 50px;*/
-  }
+.timer-container {
+  /*min-height: 50px;*/
+}
 
-  .timer {
-    height: 0.35rem !important;
-    border-radius: 0 !important;
-  }
+.timer {
+  height: 0.35rem !important;
+  border-radius: 0 !important;
+}
 
-  .timer-spinner-holder {
-    margin-bottom: 0.5rem;
+.timer-spinner-holder {
+  margin-bottom: 0.5rem;
 
-    .timer-spinner-wrapper {
-      min-height: 39px;
-      position: relative;
+  .timer-spinner-wrapper {
+    min-height: 39px;
+    position: relative;
 
+    .timer-spinner {
+      width: 2.2rem;
+      height: 2.2rem;
+    }
+
+    .timer-numbers {
+      font-size: 14px;
+      display: block;
+      position: absolute;
+      top: 6px;
+      text-align: center;
+      width: 100%;
+    }
+
+    @media only screen and (min-width: 768px) {
+      margin-top: -3px;
+      min-height: 51px;
       .timer-spinner {
-        width: 2.2rem;
-        height: 2.2rem;
+        width: 3rem;
+        height: 3rem;
       }
-
       .timer-numbers {
-        font-size: 14px;
-        display: block;
-        position: absolute;
-        top: 6px;
-        text-align: center;
-        width: 100%;
-      }
-
-      @media only screen and (min-width: 768px) {
-        margin-top: -3px;
-        min-height: 51px;
-        .timer-spinner {
-          width: 3rem;
-          height: 3rem;
-        }
-        .timer-numbers {
-          font-size: 23px;
-        }
+        font-size: 23px;
       }
     }
   }
+}
 </style>
